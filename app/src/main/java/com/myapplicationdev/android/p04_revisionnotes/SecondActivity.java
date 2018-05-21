06cd8a4f70e3d66cf072da0405326461977a6f55
16022653 <16022653@myrp.edu.sg>
2018-05-21 12:36:32 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/SecondActivity.java
@@ -0,0 +1,29 @@
+package com.myapplicationdev.android.p04_revisionnotes;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class SecondActivity extends AppCompatActivity {
+
+	ListView lv;
+	RevisionNotesArrayAdapter adapter;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_second);
+
+		lv = (ListView) this.findViewById(R.id.lv);
+
+		DBHelper dbh = new DBHelper(this);
+		ArrayList<Note> notes = dbh.getAllNotes();
+
+		adapter = new RevisionNotesArrayAdapter(this, R.layout.row, notes);
+		lv.setAdapter(adapter);
+	}
+
+
+}
